aad46f4539d43101054776b1a22246224c19a460
Zephyr <374698133@qq.com>
2017-09-12 12:38:15 +0800

    initial1

---
+++ src/main/java/com/tss/qwerty/QwertyApplication.java
@@ -0,0 +1,12 @@
+package com.tss.qwerty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QwertyApplication.class, args);
+	}
+}
